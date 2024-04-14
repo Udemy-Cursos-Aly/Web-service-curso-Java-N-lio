0079efb102ababebb0b53b9485528102c4a531ac
Alym62 <gasafm62@gmail.com>
2024-04-14 12:35:12 -0300

    Método de salvar feito.

--- src/main/java/com/udemynelio/course/services/UserService.java
+++ src/main/java/com/udemynelio/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.udemynelio.course.entities.User;
 import com.udemynelio.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,5 +20,10 @@ public class UserService {
     public User findById(Long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Esse usuário não existe: " + id));
+    }
+
+    @Transactional
+    public User insert(User entity) {
+        return repository.save(entity);
     }
 }
